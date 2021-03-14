109c37bb063364ec700f6662914f05cdb4f0f792
SDen4 <asw325@yandex.ru>
2021-03-14 20:10:05 +0500

    Head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Next_course</title>
+        <meta name="keywords" content="next.js, js" />
+        <meta name="description" content="this is nextjs_course" />
+        <meta charSet="utf-8" />
+      </Head>
+
       <h1>Hello next.js</h1>
       <Link href={'/about'}>
         <a>about</a>
