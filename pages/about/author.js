125a8660fbe355b9c56b4ef4822150c1f765be8a
SDen4 <asw325@yandex.ru>
2021-03-14 18:23:04 +0500

    features of paths and file directory (about and author)

---
+++ pages/about/author.js
@@ -0,0 +1,5 @@
+// This page available at the route: http://localhost:3000/about/author ! //
+
+export default function Author() {
+  return <h2>Author page</h2>;
+}
---
+++ pages/about/index.js
@@ -0,0 +1,5 @@
+// This page available at the route: http://localhost:3000/about ! //
+
+export default function About() {
+  return <h2>About page</h2>;
+}
